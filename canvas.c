3cae4ee853262a8011d1672bd198b31d6b4d9b35
Son-dorj <dsonam956@gmail.com>
2022-06-15 04:24:55 +0530

    added ortho to define a viewport of 1920x1080

--- canvas.c
+++ canvas.c
@@ -1,10 +1,13 @@
 #include<GL/glut.h>
+#define width 1920
+#define height 1080
 float x=0,y=0;
 void line1();
 void myInit()
 {
 	glClearColor(0,0,0,1);
 	glColor3f(1,1,1);
+	gluOrtho2D(-width/2,width/2,-height/2,height/2);
 }
 void Display()
 {
@@ -15,7 +18,7 @@ int main(int argc, char** argv)
 {
 	glutInit(&argc,argv);
 	glutInitWindowPosition(10,10);
-	glutInitWindowSize(500,500);
+	glutInitWindowSize(width,height);
 	glutCreateWindow("looping");
 	myInit();
 	glutDisplayFunc(Display);
